a5a20a5489973d9aeb60d96ea2cb795390bb948f
Robloxexploiter691 <141586656+Robloxexploiter691@users.noreply.github.com>
2025-11-26 17:17:00 -0800

    Update Main.lua

--- Main.lua
+++ Main.lua
@@ -27,7 +27,7 @@
 ]]
 
 local a={UseWorkspace=false,NoActors=false,FolderName='Sigma Spy',RepoUrl=
-'https://raw.githubusercontent.com/Dexz00/Sigma-Spy/main',ParserUrl=
+'https://raw.githubusercontent.com/Robloxexploiter691/Sigma-Spy/main',ParserUrl=
 [[https://raw.githubusercontent.com/depthso/Roblox-parser/refs/heads/main/dist/Main.luau]]
 }local b={
 ...}local c=b[1]if typeof(c)=='table'then for d,e in c do a[d]=e end end local d
